746945acd1a208ab16b215917389c1c0d4ed3c24
kowshikmeda <medakowshik8@gmail.com>
2025-09-18 15:11:52 +0530

    added cokkie to work on production

--- controllers/authController.js
+++ controllers/authController.js
@@ -123,10 +123,12 @@ const login = async (req, res) => {
   { expiresIn: '7d' }
 );
   
-res.cookie("token", token, {
-  httpOnly: true,
-  maxAge: 7 * 24 * 60 * 60 * 1000
-});
+ res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // only HTTPS in production
+      sameSite: "None", // must be None for cross-origin
+      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    });
     res.status(200).json({
       success: true,
       message: 'Login successful',
